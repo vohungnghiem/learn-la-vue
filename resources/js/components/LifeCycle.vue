1fa55a227bb3cbe6f52ac91d9126149101fa6b42
unknown <vohungnghiem@gmail.com>
2020-12-04 11:31:15 +0700

    commit lifecycle

--- resources/js/components/LifeCycle.vue
+++ resources/js/components/LifeCycle.vue
@@ -1,21 +1,40 @@
 <template>
-    <p ref="dom-element">{{counter}}</p>
+    <div class="binding-html">
+        <div class="text-title" :class="{active: isActive, hide: isHide, bold: isBold}">
+            This is text
+        </div>
+        <div>
+            <button @click="isActive = true">Active text</button>
+        </div>
+        <div>
+            <button @click="isHide = true">Hide text</button>
+        </div>
+        <div>
+            <button @click="isBold = true">Bold text</button>
+        </div>
+    </div>
 </template>
+
 <script>
-export default {
-    data() {
-        return {
-          counter: 0
+    export default {
+        data() {
+            return {
+                isActive: false,
+                isHide: false,
+                isBold: false
+            }
         }
-    },
-    updated() {
-        // Fired every second, should always be true
-        console.log(+this.$refs['dom-element'].textContent === this.counter)
-    },
-    created() {
-        setInterval(() => {
-          this.counter++
-        }, 1000)
     }
-}
 </script>
+
+<style lang="scss" scoped>
+.active {
+    color: red;
+}
+.hide {
+    display: none;
+}
+.bold {
+    font-weight: bold;
+}
+</style>
